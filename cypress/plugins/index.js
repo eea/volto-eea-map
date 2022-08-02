fe9ca3e6a98dd02c6b774f931fe73f18712e0e7b
andreiggr <andrei.ggr@gmail.com>
2022-08-02 11:20:34 +0300

    upgrade to cypress 10

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+const { defineConfig } = require('cypress');
+
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -12,15 +14,24 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  /* coverage-start
-  require('@cypress/code-coverage/task')(on, config)
-  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
-  return config
-  coverage-end */
-};
+// /**
+//  * @type {Cypress.PluginConfig}
+//  */
+// module.exports = (on, config) => {
+//   // `on` is used to hook into various events Cypress emits
+//   // `config` is the resolved Cypress config
+//   /* coverage-start
+//   require('@cypress/code-coverage/task')(on, config)
+//   on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'))
+//   return config
+//   coverage-end */
+// };
+
+module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: 'react', // or vue
+      bundler: 'webpack',
+    },
+  },
+});
