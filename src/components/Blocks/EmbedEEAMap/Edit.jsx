719c00776dff7ba63bbd57761a2c86ec5345a911
andreiggr <andrei.ggr@gmail.com>
2022-08-22 14:09:01 +0300

    update embed map with data_query

--- src/components/Blocks/EmbedEEAMap/Edit.jsx
+++ src/components/Blocks/EmbedEEAMap/Edit.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { SidebarPortal } from '@plone/volto/components';
+
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+import { getContent } from '@plone/volto/actions';
+
 import BlockDataForm from '@plone/volto/components/manage/Form/BlockDataForm';
-import { Schema } from './Schema';
 import View from './View';
+import { Schema } from './Schema';
 import { addPrivacyProtectionToSchema } from '@eeacms/volto-embed';
 import './styles/map.css';
 
 const Edit = (props) => {
   const { block, data, onChangeBlock, selected, id } = props;
   const schema = React.useMemo(() => Schema(props), [props]);
+
+  React.useEffect(() => {
+    props.getContent(data.vis_url, null, id);
+    //    eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data.vis_url]);
+
+  React.useEffect(() => {
+    if (
+      !data.data_query_params ||
+      props.data_query !== data.data_query_params
+    ) {
+      onChangeBlock(block, {
+        ...data,
+        data_query_params: props.data_query,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.data_query, block, data]);
 
   return (
     <div>
@@ -31,4 +55,15 @@ const Edit = (props) => {
   );
 };
 
-export default Edit;
+export default compose(
+  connect(
+    (state, props) => ({
+      data_query: state.content.data.data_query,
+      data_provenance:
+        state.content.subrequests?.[props.id]?.data?.data_provenance,
+    }),
+    {
+      getContent,
+    },
+  ),
+)(Edit);
