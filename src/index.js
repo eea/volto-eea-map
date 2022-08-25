6bb7daf458a47bd185870ec3627a542f8ba715bc
andreiggr <andrei.ggr@gmail.com>
2022-08-25 16:07:53 +0300

    only embed viz for now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {
-  EEAMapEdit,
-  EEAMapView,
+  // EEAMapEdit,
+  // EEAMapView,
   EmbedMapView,
   EmbedMapEdit,
 } from '@eeacms/volto-eea-map/components';
@@ -34,33 +34,33 @@ export default (config) => {
 
   // EEA MAP BLOCK
 
-  config.blocks.blocksConfig.eea_map_block = {
-    id: 'eea_map_block', //  The name (id) of the block
-    title: 'EEA Map', //  The display name of the block
-    icon: world, //  The icon used in the block chooser
-    group: 'common', //The group (blocks can be grouped, displayed in the chooser)
-    view: EEAMapView, //The view mode component
-    edit: EEAMapEdit, // The edit mode component
-    sidebarTab: 1, // The sidebar tab you want to be selected when selecting the block
-    security: {
-      addPermission: [], //  Future proof (not implemented yet) add user permission role(s)
-      view: [], //Future proof (not implemented yet) view user role(s)
-    },
-    variations: [
-      {
-        id: 'default',
-        title: 'EEA Map (default)',
-        isDefault: true,
-        view: EEAMapView,
-      },
-      {
-        id: 'extra',
-        title: 'Extra variation (expand if needed)',
-        isDefault: true,
-        view: EEAMapView,
-      },
-    ],
-  };
+  // config.blocks.blocksConfig.eea_map_block = {
+  //   id: 'eea_map_block', //  The name (id) of the block
+  //   title: 'EEA Map', //  The display name of the block
+  //   icon: world, //  The icon used in the block chooser
+  //   group: 'common', //The group (blocks can be grouped, displayed in the chooser)
+  //   view: EEAMapView, //The view mode component
+  //   edit: EEAMapEdit, // The edit mode component
+  //   sidebarTab: 1, // The sidebar tab you want to be selected when selecting the block
+  //   security: {
+  //     addPermission: [], //  Future proof (not implemented yet) add user permission role(s)
+  //     view: [], //Future proof (not implemented yet) view user role(s)
+  //   },
+  //   variations: [
+  //     {
+  //       id: 'default',
+  //       title: 'EEA Map (default)',
+  //       isDefault: true,
+  //       view: EEAMapView,
+  //     },
+  //     {
+  //       id: 'extra',
+  //       title: 'Extra variation (expand if needed)',
+  //       isDefault: true,
+  //       view: EEAMapView,
+  //     },
+  //   ],
+  // };
 
   config.blocks.blocksConfig.embed_eea_map_block = {
     id: 'embed_eea_map_block', // The name (id) of the block
