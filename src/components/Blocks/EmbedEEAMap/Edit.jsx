a6263d54374719914e6f52ee8e3ae7e3cc9450f5
andreiggr <andrei.ggr@gmail.com>
2022-11-11 13:08:35 +0200

    blocks styles for embed map block

--- src/components/Blocks/EmbedEEAMap/Edit.jsx
+++ src/components/Blocks/EmbedEEAMap/Edit.jsx
@@ -4,6 +4,8 @@ import { SidebarPortal } from '@plone/volto/components';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 
+import { BlockStyleWrapperEdit } from '@eeacms/volto-block-style/BlockStyleWrapper';
+import cx from 'classnames';
 import { getContent } from '@plone/volto/actions';
 
 import BlockDataForm from '@plone/volto/components/manage/Form/BlockDataForm';
@@ -58,7 +60,20 @@ const Edit = (props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.block, props.data_query, data.data_query_params]);
   return (
-    <div>
+    <BlockStyleWrapperEdit
+      {...props}
+      role="presentation"
+      data={{
+        ...(props.data || {}),
+        styles: {
+          ...(props.data?.styles || {}),
+          customClass: cx(
+            props.data?.styles?.customClass || '',
+            'custom-map-class',
+          ),
+        },
+      }}
+    >
       <View data={data} id={id} isEdit={true} />
       <SidebarPortal selected={selected}>
         <BlockDataForm
@@ -74,7 +89,7 @@ const Edit = (props) => {
           formData={data}
         />
       </SidebarPortal>
-    </div>
+    </BlockStyleWrapperEdit>
   );
 };
 
