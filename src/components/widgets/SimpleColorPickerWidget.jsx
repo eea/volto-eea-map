3fac75391758ad6b24b6e54bb5f7d19c373f9fba
Andrei Grigore <andrei.ggr@gmail.com>
2023-06-14 00:10:39 +0300

    fix: colorpicker with submit  #254131

--- src/components/widgets/SimpleColorPickerWidget.jsx
+++ src/components/widgets/SimpleColorPickerWidget.jsx
@@ -3,6 +3,7 @@ import { FormFieldWrapper, Icon } from '@plone/volto/components';
 import { Button } from 'semantic-ui-react';
 import loadable from '@loadable/component';
 import clearSVG from '@plone/volto/icons/clear.svg';
+import checkSVG from '@plone/volto/icons/check.svg';
 
 const ReactColor = loadable.lib(() => import('react-color'));
 
@@ -10,13 +11,29 @@ export default (props) => {
   const { id, value, onChange, available_colors } = props;
   const [showPicker, setShowPicker] = React.useState(false);
 
+  const [color, setColor] = React.useState(value);
+
+  const handleChangeColor = (valColor) => {
+    setColor(valColor.hex);
+  };
+
+  const handleConfirmColor = () => {
+    onChange(id, color);
+    setShowPicker(false);
+  };
+
+  const handleAbortColor = () => {
+    setColor(value);
+    setShowPicker(false);
+  };
+
   return (
     <FormFieldWrapper
       {...props}
       draggable={false}
       className="simple-color-picker-widget"
     >
-      <div>
+      <div style={{ position: 'relative' }}>
         <Button.Group fluid>
           <Button
             color={value}
@@ -35,27 +52,60 @@ export default (props) => {
           >
             <Icon name={clearSVG} size="18px" color="red" />
           </Button>
+          {showPicker ? (
+            <div
+              style={{
+                position: 'absolute',
+                top: '0',
+                zIndex: '77',
+                background: 'lightgrey',
+                display: 'flex',
+                flexDirection: 'column',
+                padding: '5px',
+                borderRadius: '2px',
+              }}
+            >
+              <ReactColor>
+                {({ SketchPicker }) => {
+                  return (
+                    <SketchPicker
+                      width="180px"
+                      colors={available_colors}
+                      color={color || '#000'}
+                      onChangeComplete={(value) => {
+                        // setShowPicker(false);
+                        //onChange(id, value.hex);
+                        handleChangeColor(value);
+                      }}
+                    ></SketchPicker>
+                  );
+                }}
+              </ReactColor>
+              <Button.Group>
+                <Button
+                  size="tiny"
+                  compact
+                  title="Reset color"
+                  style={{ paddingLeft: '8px', paddingRight: '0px' }}
+                  onClick={() => handleAbortColor()}
+                >
+                  <Icon name={clearSVG} size="18px" />
+                </Button>
+                <Button
+                  onClick={() => handleConfirmColor()}
+                  color="green"
+                  compact
+                  size="tiny"
+                  title="Confirm color"
+                >
+                  <Icon name={checkSVG} size="18px" />
+                </Button>
+              </Button.Group>
+            </div>
+          ) : (
+            ''
+          )}
         </Button.Group>
-
-        {showPicker ? (
-          <ReactColor>
-            {({ SketchPicker }) => {
-              return (
-                <SketchPicker
-                  width="180px"
-                  colors={available_colors}
-                  color={value || '#000'}
-                  onChangeComplete={(value) => {
-                    setShowPicker(false);
-                    onChange(id, value.hex);
-                  }}
-                ></SketchPicker>
-              );
-            }}
-          </ReactColor>
-        ) : (
-          ''
-        )}
       </div>
     </FormFieldWrapper>
   );
