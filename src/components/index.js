018464116ba5b86b2c1e0a4ebc9b2fd95b772b66
Alin Voinea <contact@avoinea.com>
2023-08-29 20:37:12 +0300

    test: Fix eslint and yarn i18n

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export Webmap from './Webmap';
-export ExtraViews from './ExtraViews';
-export LegendView from './LegendView';
+export { default as Webmap } from './Webmap';
+export { default as ExtraViews } from './ExtraViews';
+export { default as LegendView } from './LegendView';
 
-export EmbedMapView from './Blocks/EmbedEEAMap/View';
-export EmbedMapEdit from './Blocks/EmbedEEAMap/Edit';
+export { default as EmbedMapView } from './Blocks/EmbedEEAMap/View';
+export { default as EmbedMapEdit } from './Blocks/EmbedEEAMap/Edit';
